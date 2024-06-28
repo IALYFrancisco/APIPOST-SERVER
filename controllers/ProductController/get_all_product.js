553c86193ae651ad2b30110c4baf2d0e77e51b56
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-06-28 08:15:21 +0300

    ajout d'autorisation CORS sur l'endpoint all_users

--- controllers/ProductController/get_all_product.js
+++ controllers/ProductController/get_all_product.js
@@ -1,5 +1,6 @@
 import Product from '../../models/product_models.js'
 
 export const get_all_product = async (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
     response.status(200).send( await Product.find() );
 }
