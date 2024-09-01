80cd3e0aa2984dd23a0533688da6af63e53f9bb7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-09-01 09:25:25 +0300

    ajout d'autorisation CORS Methods de toutes méthodes http dans le middleware de l'application

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use((request, response, next) => {
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Allow-Methods', '*')
     next();
 })
 
