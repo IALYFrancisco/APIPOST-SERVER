4bb13a2266ffd0ba0906174245cd80e1d84dc0ea
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-02-03 21:07:04 +0300

    suppression d'import inutil dns le fichier point d'entrée index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import dbConnexion from './services/db_connexion.js';
 import product_router from './routes/product_route.js';
 
 dotenv.config();
