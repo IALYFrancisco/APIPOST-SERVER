9cf0e528ac9ec73fdad6decd1d73f7d1a4fc2599
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-02-03 21:51:36 +0300

    optimisation du méthode de récupération de la liste de tout les proudits

--- controllers/ProductController/get_all_product.js
+++ controllers/ProductController/get_all_product.js
@@ -1,6 +1,9 @@
 import Product from '../../models/product_models.js'
+import { dbConnexion, dbDisconnexion } from '../../services/dbServices.js';
 
 export const get_all_product = async (request, response) => {
+    await dbConnexion()
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.status(200).send( await Product.find() );
+    await dbDisconnexion()
 }
