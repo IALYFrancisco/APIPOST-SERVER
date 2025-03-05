e9e3b33b7a9ca9aea5773122f00feeade19526fc
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-05 17:02:26 +0300

    amélioration de service d'nevoie d'email

--- controllers/ProductController/sendEmail.js
+++ controllers/ProductController/sendEmail.js
@@ -2,5 +2,5 @@ import { sendEmailServices } from "../../services/sendEmailService.js";
 
 export const sendEmail = async (request, response) => {
     response.set("Content-Type", "application/json");
-    sendEmailServices(request.body);
+    await sendEmailServices(request.body);
 }
--- services/sendEmailService.js
+++ services/sendEmailService.js
@@ -1,28 +1,34 @@
 import axios from 'axios';
 
 export async function sendEmailServices(dataTosend){
-    await axios({
-        method: 'POST',
-        url: "https://api.brevo.com/v3/smtp/email",
-        data: dataTosend
-        // {
-        //     name: "Test email",
-        //     sender: {
-        //         "name": "IALY Francisco Raymond",
-        //         "email": "ialyfrancisco7@gmail.com"
-        //     },
-        //     to: [{
-        //         "email": "franciscoialy43@gmail.com"
-        //     }],
-        //     subject: "Our discussion",
-        //     htmlContent: "This the content"
-        // }
-        ,
-        headers: {
-            "Content-Type":"application/json",
-            "api-key": process.env.API_KEY
-        }
-    }).then((response)=>{
-        console.log(response)
-    }).catch(error => console.log(error))
+    try {
+        await axios({
+            method: 'POST',
+            url: "https://api.brevo.com/v3/smtp/email",
+            data: dataTosend
+            // {
+            //     name: "Test email",
+            //     sender: {
+            //         "name": "IALY Francisco Raymond",
+            //         "email": "ialyfrancisco7@gmail.com"
+            //     },
+            //     to: [{
+            //         "email": "franciscoialy43@gmail.com"
+            //     }],
+            //     subject: "Our discussion",
+            //     htmlContent: "This the content"
+            // }
+            ,
+            headers: {
+                "Content-Type":"application/json",
+                "api-key": process.env.API_KEY
+            }
+        }).then((response)=>{
+            return response
+        }).catch((error) => {
+            return error
+        })
+    }catch(error){
+        return error
+    }
 }
