d644623d99bc9818c7c2075eabf7c9f7890ee009
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-05 17:07:02 +0300

    amélioration du contrô d'envoie d'email

--- controllers/ProductController/sendEmail.js
+++ controllers/ProductController/sendEmail.js
@@ -2,5 +2,10 @@ import { sendEmailServices } from "../../services/sendEmailService.js";
 
 export const sendEmail = async (request, response) => {
     response.set("Content-Type", "application/json");
-    await sendEmailServices(request.body);
+    brevoRequestResponse = await sendEmailServices(request.body);
+    if(brevoRequestResponse){
+        response.status(200).json("Email sent 📩✅")
+    }else {
+        response.status(500).json("Error in the server ⛔💻")
+    }
 }
