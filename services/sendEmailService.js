959da5ee60cada0f426e5b0fc909b95c66b89da7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-29 08:20:28 +0300

    suppréssion de codes inutiles

--- services/sendEmailService.js
+++ services/sendEmailService.js
@@ -5,20 +5,7 @@ export async function sendEmailServices(dataTosend){
         await axios({
             method: 'POST',
             url: "https://api.brevo.com/v3/smtp/email",
-            data: dataTosend
-            // {
-            //     name: "Test email",
-            //     sender: {
-            //         "name": "IALY Francisco Raymond",
-            //         "email": "ialyfrancisco7@gmail.com"
-            //     },
-            //     to: [{
-            //         "email": "franciscoialy43@gmail.com"
-            //     }],
-            //     subject: "Our discussion",
-            //     htmlContent: "This the content"
-            // }
-            ,
+            data: dataTosend,
             headers: {
                 "Content-Type":"application/json",
                 "api-key": process.env.API_KEY
