877f836556b816ad26a73792995870325dd0883a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-25 09:05:07 +0300

    amélioration de texte de lancement de l'apllication

--- index.js
+++ index.js
@@ -21,4 +21,4 @@ app.get('/', (request, response) => {
 
 app.use('/', product_router);
 
-app.listen(process.env.APP_PORT, ()=>{ console.log(`http://127.0.0.1:${process.env.APP_PORT}`) });
+app.listen(process.env.APP_PORT, ()=>{ console.log(`The application is listening on ${process.env.APP_HOST}`) });
