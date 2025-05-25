92c9fd485103de5c5ae8a4ffe8870ac76eebe14b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-25 09:35:22 +0300

    création de modèle de données pour les chansons

---
+++ models/Songs.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const SongsSchema = new mongoose.Schema({
+    title : { type : String, required : true },
+    weight : { type : String },
+    duration : { type : String },
+    author: { type: String, required: true },
+    file_url : { type: String, required: true },
+    album : { type : String },
+    add_date : { type : Date, required: true, default: Date.now }
+})
+
+export const Songs = new mongoose.Model("Songs", SongsSchema)
